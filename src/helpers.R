307395f8d7ac2c7327901de9a8b2e248f78004f2
Gerold Csendes <gerold.csendes@gmail.com>
2020-06-03 12:05:23 +0200

    add scraper

--- src/helpers.R
+++ src/helpers.R
@@ -77,11 +77,15 @@ my_secrets <- function() {
 }
 
 # get geocodes for df
-geocode_df <- function(df) {
+geocode_df <- function(df, address_var) {
+  
+  # assign lat, lon cols
+  df$lat <- c()
+  df$lon <- c()
   
   for (row in 1:nrow(df)) {
     
-    address <- as.character(df[row, "cim"])
+    address <- as.character(df[row, address_var])
     
     print(row)
     print(address)
@@ -99,4 +103,57 @@ geocode_df <- function(df) {
     )
   }
   return(df)
+}
+
+# scrape data from ingatlan.com
+scrape_real_estate <- function(page) {
+  
+  print(page)
+  
+  # different user agent
+  uastring <- "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+  httr::user_agent(uastring)
+  
+  tryCatch ({
+    # query server
+    res <- httr::GET("https://ingatlan.com/lista/elado+lakas+Budapest", user_agent("httr"),
+                     query = list(
+                       page = page))
+    
+    # parse html
+    t <- read_html(res)
+    
+    price <- t %>% 
+      html_nodes(".price") %>% 
+      html_text()
+    
+    price_sqm <- t %>% 
+      html_nodes(".price--sqm") %>% 
+      html_text()
+    
+    address <- t %>% 
+      html_nodes(".listing__address") %>% 
+      html_text()
+    
+    area <- t %>% 
+      html_nodes(".listing__data--area-size") %>% 
+      html_text()
+    
+    df <- data.frame(price = price,
+                     price_sqm = price_sqm,
+                     address = address,
+                     area = area,
+                     page=c(page))},
+    
+    error = function(e){
+      print(paste("page", page, "not found"))
+      df <- data.frame(page=c(page),
+                       price = NA,
+                       price_sqm = NA,
+                       address = NA,
+                       area = NA)
+    }
+  )
+  Sys.sleep(1) # doze some time
+  return (df)
 }
